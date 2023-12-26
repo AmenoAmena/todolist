1e82ec9163457ab28c42c28154f5c84b3e0493f2
AmenoAmena <ardacavusoglu666@gmail.com>
2023-12-26 19:56:57 +0300

    Basic to do list

--- app.js
+++ app.js
@@ -1,34 +1,30 @@
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
+let tasks = [];
+let orderedList = document.getElementById("orderedList"); 
+
+
+
+for (let i = 0; i < tasks.length; i++) {
+    orderedList.innerHTML += "<li class='li'>" + tasks[i] + "</li>";
+}
+
+
 function addTask() {
-    if(inputBox.value === "" ){
-        alert("You must write something!");
+    let formValue = document.getElementById("box").value;
+    
+
+    if (formValue.length === 0) {
+        alert("There is no task");
     }
-    else{
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML =  "\u00d7";
-        li.appendChild(span);
+    else if (formValue.length > 30) {
+        alert("Should be smaller than 30 characters")
+        document.getElementById("box").value = "";    
     }
-    inputBox.value = "";
-    saveData();
-}   
-listContainer.addEventListener("click",function(e) {
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData();
-    }
-    else if(e.target.tagName === "SPAN") {
-        e.target.parentElement.remove();
-        saveData();
-    }
-},false)
-function saveData() {
-    localStorage.setItem("data",listContainer.innerHTML)
+    
+
+    else {
+        tasks.push(formValue);
+        orderedList.innerHTML += "<li>" + formValue + "</li>"
+        document.getElementById("box").value = "";    
+    
+        }
 }
-function showTask() {
-    listContainer.innerHTML = localStorage.getItem("data");
-}
-showTask();
