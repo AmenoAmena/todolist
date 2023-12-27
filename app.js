933d0484ce273f2b5a5e9337f3d7978c26c5f74f
AmenoAmena <ardacavusoglu666@gmail.com>
2023-12-27 17:26:47 +0300

    final product

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
 let tasks = [];
-let orderedList = document.getElementById("orderedList"); 
+let orderedList = document.getElementById("orderedList");
 
+function renderTasks() {
+    orderedList.innerHTML = "";
+    for (let i = 0; i < tasks.length; i++) {
+        let listItem = document.createElement("li");
+        listItem.textContent = tasks[i];
+        
+        let deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.id='deleteButton';
+        deleteButton.addEventListener("click", function() {
+            deleteTask(i);
+        });
 
-
-for (let i = 0; i < tasks.length; i++) {
-    orderedList.innerHTML += "<li class='li'>" + tasks[i] + "</li>";
+        listItem.appendChild(deleteButton);
+        orderedList.appendChild(listItem);
+    }
 }
-
 
 function addTask() {
     let formValue = document.getElementById("box").value;
-    
 
     if (formValue.length === 0) {
         alert("There is no task");
-    }
-    else if (formValue.length > 30) {
-        alert("Should be smaller than 30 characters")
-        document.getElementById("box").value = "";    
-    }
-    
-
-    else {
+    } else if (formValue.length > 25) {
+        alert("Should be smaller than 25 characters");
+        document.getElementById("box").value = "";
+    } else {
         tasks.push(formValue);
-        orderedList.innerHTML += "<li>" + formValue + "</li>"
-        document.getElementById("box").value = "";    
-    
-        }
+        renderTasks();
+        document.getElementById("box").value = "";
+    }
+}
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    renderTasks();
+    console.log("Task deleted at index: " + index);
 }
